cec34f9352bab43d46a896c124f23ca08e652a9a
Fred Sauer <fredsa@google.com>
2012-05-29 21:10:28 -0700

    also debug user service state

--- src/echo/EchoServlet.java
+++ src/echo/EchoServlet.java
@@ -8,10 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 @SuppressWarnings("serial")
 public class EchoServlet extends HttpServlet {
   private static final Logger logger = Logger.getLogger(EchoServlet.class.getName());
-  
+
   @Override
   public void service(HttpServletRequest request, HttpServletResponse resp) throws IOException {
     resp.setContentType("text/plain");
@@ -27,5 +31,18 @@ public class EchoServlet extends HttpServlet {
       logger.info(msg);
     }
 
+    resp.getWriter().println("--------------------------------------");
+    UserService users = UserServiceFactory.getUserService();
+    boolean userLoggedIn = users.isUserLoggedIn();
+    resp.getWriter().println("userLoggedIn: " + userLoggedIn);
+    if (userLoggedIn) {
+      User currentUser = users.getCurrentUser();
+      boolean userIsAdmin = users.isUserAdmin();
+      resp.getWriter().println("currentUser : " + currentUser);
+      resp.getWriter().println("userIsAdmin : " + userIsAdmin);
+    } else {
+      String loginUrl = users.createLoginURL("/");
+      resp.getWriter().println("loginUrl : " + loginUrl);
+    }
   }
 }
